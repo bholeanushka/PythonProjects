55f8ed6f46a3c7b4f49cd7cfaef059695f2a830d
Anushka Bhole <105558175+bholeanushka@users.noreply.github.com>
2023-12-16 16:19:14 +0530

    Update GuessingGame.py

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,3 +1,31 @@
+from tkinter import *
+import random
+def clear_text(event):
+    if entry.get()=="Enter the guess":
+        entry.delete(0,END)
+
+def guess(random_num,x):
+    
+    guess=x
+    #while guess!=random_num:
+        #guess=int(input("Guess a number from 1 to 10 : "))
+    if random_num < guess :
+        res="Oooopssss,Try again ! your Guess is too high."
+    elif random_num > guess :
+        res="Oooopssss,Try again ! your Guess is too low."
+    else :
+        res="Yayyy! you guess it....✨"
+
+    return res
+
+def get_value():
+    global random_num
+    if resultlabel.cget("text")=="Yayyy! you guess it....✨":
+        random_num=random.randint(1,10)
+        
+    val=int(entry.get())
+    result=guess(random_num,val)
+    resultlabel.config(text=result)
 window=Tk()
 window.title("Guessing Game")
 window.minsize(300,300)
